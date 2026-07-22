0f08b3eb4963e93e3f6f8c557b986f6693025843
Paul Wighton <paul.wighton@gmail.com>
2026-07-22 17:39:26 -0400

    [WIP] constants refactor

--- petsurfer_km/methods.py
+++ petsurfer_km/methods.py
@@ -9,3 +9,23 @@ implies also running MRTM1
 from __future__ import annotations
 
 KM_METHOD_ORDER = ["suvr", "mrtm1", "mrtm2", "logan", "logan-ma1", "patlak"]
+
+# BEP023 model labels
+MODEL_LABELS = {
+    "suvr": "SUVR",
+    "mrtm1": "MRTM1",
+    "mrtm2": "MRTM2",
+    "logan": "Logan",
+    "logan-ma1": "MA1",
+    "patlak": "Patlak",
+}
+
+# Primary measurement per method
+MEAS_LABELS = {
+    "suvr": "SUVR",
+    "mrtm1": "BPND",
+    "mrtm2": "BPND",
+    "logan": "VT",
+    "logan-ma1": "VT",
+    "patlak": "Ki",
+}
